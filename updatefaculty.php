02096fadbd7679ad2ecd72df23f6ff6a6548ecee
mahima-mahendru <73685905+mahima-mahendru@users.noreply.github.com>
2021-11-28 11:55:39 +0530

    Update updatefaculty.php

--- updatefaculty.php
+++ updatefaculty.php
@@ -8,13 +8,59 @@ header('Location:AdminLogin.php');
 
 $userid = $_SESSION["umail"];
 ?><?php include('after-login.php'); ?>
-	<div class="container">
-     <div class="row">
-			<div class="col-md-3"></div>
 
-			<div class="col-md-6">
-				<h3 class="page-header">Welcome <a href="welcomeadmin">Admin</a> </h3>
-				<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Classroom-Aid</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  
+</head>
+
+<body>
+
+ 
+  <main id="main">
+
+    <!-- ======= Breadcrumbs Section ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+        <h3 class="page-header">Welcome <a href="welcomeadmin">Admin</a> </h3>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs Section -->
+
+    <section class="inner-page">
+      <div class="container">
+      <?php
 				include("database.php");
 				$new2=$_GET['fid'];
 				
@@ -71,7 +117,7 @@ $userid = $_SESSION["umail"];
 				{
 					$_SESSION['status']="Updated Successfully!";
 					$_SESSION['status_code']="success";
-					
+		
 				}
 				else
 				{
@@ -86,9 +132,33 @@ $userid = $_SESSION["umail"];
 
 			} 
 				?>
-            </div>
-			
 
-			<div class="col-md-3"></div>
-	</div>
-	<?php include('alert.php'); ?>
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+ <!-- ======= Footer ======= -->
+ <footer id="footer">
+    
+   
+</footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  <?php include('alert.php'); ?>
+
+</body>
+
+</html>

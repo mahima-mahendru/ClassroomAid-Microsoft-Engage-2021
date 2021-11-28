046184e8200ebaac40361b06c7378eab256746ed
mahima-mahendru <73685905+mahima-mahendru@users.noreply.github.com>
2021-11-28 11:17:17 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,16 +1,11 @@
 <?php
 
-    // $server = "localhost";
-    // $user = "root";
-    // $passdb = "";
-    // $db = "classroomaid2";
-    // $connect = mysqli_connect( $server, $user, $passdb, $db )or die( "Connection Error" );
-
-    $server = "remotemysql.com";
-    $user = "2KNmQb9QLs";
-    $passdb = "31JXVZj0b6";
-    $db = "2KNmQb9QLs";
+    $server = "localhost";
+    $user = "root";
+    $passdb = "";
+    $db = "classroomaid2";
     $connect = mysqli_connect( $server, $user, $passdb, $db )or die( "Connection Error" );
 
+  
 
 ?>

71c0676398a1b779d0c2bd59fec2747666976a16
mahima-mahendru <73685905+mahima-mahendru@users.noreply.github.com>
2021-11-28 16:26:05 +0530

    Update logoutfaculty.php

--- logoutfaculty.php
+++ logoutfaculty.php
@@ -1,8 +1,14 @@
-<?php
+<!-- <?php
 session_start();
 ?>
 <?php
 $_SESSION["fidx"]=="";
 session_unset('fidx');
 header('Location:index.php');
+?> -->
+
+<?php
+session_start();
+unset($_SESSION["fidx"]);
+header('Location:index');
 ?>
